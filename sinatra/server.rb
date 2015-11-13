d4fcf224968eab05d8fd1b6037719ed7e01d4603
Rafa Ramírez <raffe90@gmail.com>
2015-11-13 18:39:03 +0100

    Implements views

--- sinatra/server.rb
+++ sinatra/server.rb
@@ -3,3 +3,15 @@ require "sinatra"
 get "/" do
 	"My first Sinatra app."
 end
+
+get '/hi' do
+  "Hello World!"
+end
+
+get '/about' do
+  "This is a brief bio."
+end
+
+get "/author" do
+	erb(:author)
+end
